1b932dffd8ffad838271ece2567d8af5b008f93d
1barizon <joao.mello@grad.iprj.uerj.br>
2025-11-27 22:24:57 -0300

    classe grafo

--- src/graph.py
+++ src/graph.py
@@ -1,6 +1,105 @@
 import networkx as nx
+import os
+from scipy.io import mmread
+import numpy as np
 
 
 class GraphInstance:
-    def __init__(self, filepaht:str):
-        pass
+    def __init__(self, filepath:str):
+        print(f"Carregando grafo: {filepath}...")
+        self.path = filepath
+        print(self.path)
+        self.graph = self.load_graph()
+        self.reqs = []
+        self.calc_req()
+        self.sol = np.zeros(shape=(len(self.graph.nodes)), dtype=np.int32)
+        self.adj_mat = nx.adjacency_matrix(self.graph)
+        
+
+
+    def load_graph(self):
+        """
+        Carrega grafos de arquivos .txt, .edges, .edge, .csv, .gml ou mtx.
+        Detecta automaticamente:
+        1. Delimitadores: Espaço, Vírgula (CSV) ou Tabulação.
+        2. Comentários: '#' ou '%'.
+        3. Cabeçalhos de texto (ex: id_1, id_2) e os remove.
+        """
+        if not os.path.exists(self.path):
+            raise FileNotFoundError(f"Arquivo não encontrado: {self.path}")
+
+        # Se for GML, usa o leitor específico
+        if self.path.endswith('.gml'):
+            return nx.read_gml(self.path, label='id')
+        
+        if self.path.endswith(".mtx"):
+            sparse_matrix = mmread(self.path)
+            G = nx.from_scipy_sparse_array(sparse_matrix)
+            g = G.to_undirected()
+            G.remove_edges_from(nx.selfloop_edges(G))
+            return G
+
+
+        # Configurações padrão
+        delimiter = None 
+        comment_char = '#'
+        has_header = False # Nova flag para controle
+        
+        # 1. ESPIAR O ARQUIVO (Detecção de metadados e cabeçalho)
+        with open(self.path, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if not line: continue 
+                
+                # Se for comentário explícito, detectamos o caractere e seguimos
+                if line.startswith(('%', '#')):
+                    comment_char = line[0]
+                    continue
+                
+            
+                # Detecta separador
+                if ',' in line: delimiter = ','
+                elif '\t' in line: delimiter = '\t'
+                else: delimiter = None # Espaço
+
+                # Teste de conversão: Tenta converter o primeiro elemento para int
+                parts = line.split(delimiter) if delimiter else line.split()
+                try:
+                    int(parts[0]) # Se conseguir virar número, é dado!
+                    has_header = False
+                except ValueError:
+                    has_header = True
+                
+                break 
+
+        print(f"Lendo {self.path} | Delim: '{'espaço' if delimiter is None else delimiter}' | Header: {has_header}")
+
+        # 2. LER O ARQUIVO (Pulando o cabeçalho se necessário)
+        try:
+            with open(self.path, 'r') as f:
+                if has_header:
+                    next(f)
+                G = nx.read_edgelist(
+                    f, 
+                    nodetype=int,          
+                    comments=comment_char, 
+                    delimiter=delimiter,   
+                    create_using=nx.Graph(), 
+                    data=False             
+                )
+            
+            G.remove_edges_from(nx.selfloop_edges(G))
+            return G
+            
+        except Exception as e:
+            raise ValueError(f"Erro crítico ao ler {self.path}: {e}")
+        
+    def calc_req(self):
+        for node, grau in self.graph.degree():
+            req = (grau+1)//2
+            self.graph.nodes[node]["requisito"] = req
+            self.reqs.append(req)
+
+       
+
+    
---
+++ test/test_graph_class.py
@@ -0,0 +1,15 @@
+import sys
+import os
+from scipy.io import mmread
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.graph import GraphInstance
+import networkx as nx
+
+
+
+file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'football.gml')
+file_path = os.path.normpath(file_path)
+file_path = os.path.abspath(file_path)
+instancia = GraphInstance(file_path)
+print(instancia.sol, instancia.reqs)
+
