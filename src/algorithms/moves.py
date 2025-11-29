606a4dbab4d0a15f37ff50d12aa8fd934bdc3314
1barizon <joao.mello@grad.iprj.uerj.br>
2025-11-29 17:21:18 -0300

    vizinhancas

--- src/algorithms/moves.py
+++ src/algorithms/moves.py
@@ -3,14 +3,41 @@ import random
 
 # funcoes de pertubacoes
 
+
 def remove_fix(solution, graph:GraphInstance):
     new_solution = solution.copy() 
     while graph.is_solution(new_solution):
         index_sol = [i for i in range(len(solution)) if new_solution[i] == 1]
         node_to_remove = random.choice(index_sol)
         new_solution[node_to_remove] = 0
+    while graph.is_solution(new_solution) == False:
+        out_index = [i for i in range(len(solution)) if new_solution[i] == 0]
+        node_to_add = random.choice(out_index)  # devo adicionar de forma gulosa ou aleatoria ?
+        new_solution[node_to_add] = 1 
+    return new_solution
+
+
+def add_remove(solution, graph:GraphInstance):
+    new_solution = solution.copy()
     out_index = [i for i in range(len(solution)) if new_solution[i] == 0]
-    for node in out_index: # talvez adicionar e aceitar somente o que melhora ? ou aceitar piora? 
-        new_solution[node] = 1 
-        if graph.is_solution(new_solution):
-            return new_solution
+    node_to_add = random.choice(out_index)  # devo adicionar de forma gulosa ou aleatoria ?
+    new_solution[node_to_add] = 1 
+    while True: 
+        sol_ = new_solution.copy()
+        index_sol = [i for i in range(len(solution)) if new_solution[i] == 1]
+        if not index_sol:
+            break
+        node_to_remove = random.choice(index_sol)
+        sol_[node_to_remove] = 0
+        if graph.is_solution(sol_):
+            new_solution = sol_
+        else:
+            break
+    return new_solution
+
+    
+
+
+
+
+
--- test/testes.py
+++ test/testes.py
@@ -5,8 +5,10 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.graph import GraphInstance
 import networkx as nx
 from src.algorithms.greedy import Guloso
-from src.algorithms.moves import remove_fix
+from src.algorithms.moves import remove_fix, add_remove
 
+import matplotlib.pyplot as plt
+import numpy as np 
 
 file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'football.gml')
 file_path = os.path.normpath(file_path)
@@ -15,9 +17,25 @@ instancia = GraphInstance(file_path)
 
 
 sol = Guloso(instancia)
-shake = remove_fix(sol, instancia)
-print(shake.sum())
+stats = [[], []]
+for i in range(100):
+    mov1 = remove_fix(sol, instancia)
+    mov2 = add_remove(sol, instancia)
+    stats[0].append(mov1.sum()-sol.sum())
+    stats[1].append(mov2.sum()-sol.sum())
 
 
 
+count_neg1 = 0
+for i in stats[0]:
+    if i < 0:
+        count_neg1 += 1
 
+
+count_neg2 = 0
+for i in stats[1]:
+
+    if i < 0:
+        count_neg2 += 1
+
+print(count_neg1, count_neg2)
